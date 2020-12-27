eee8bb11278052aed298c35b65e7e1473e380efa
Ricardo Godoy <rq.godoy@gmail.com>
2020-12-27 01:46:24 -0300

    Adição de fundos

---
+++ Assets/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+  public static GameManager instance;
+  [SerializeField] private float currentFunds = 200f;
+  public TextMeshProUGUI funds;
+
+  private void Awake()
+  {
+    if (instance == null) instance = this;
+    if (instance != this) DestroyImmediate(this);
+    UpdateFundCanvas();
+  }
+
+  private bool HasFunds(float amout)
+  {
+    return amout <= currentFunds;
+  }
+
+  public void AddFunds(float amount)
+  {
+    currentFunds += amount;
+    UpdateFundCanvas();
+  }
+
+  private void UpdateFundCanvas()
+  {
+    funds.text = $"Fundos R$ {currentFunds.ToString("F2")}";
+  }
+}
