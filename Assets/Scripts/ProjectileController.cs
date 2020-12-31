f350ab4679ff3bde26d65cf9673d3d9875150ba1
Ricardo Godoy <rq.godoy@gmail.com>
2020-12-31 02:16:44 -0300

    Correção no projetil

--- Assets/Scripts/ProjectileController.cs
+++ Assets/Scripts/ProjectileController.cs
@@ -9,22 +9,39 @@ public class ProjectileController : MonoBehaviour
   public float acceleration = 0.1f;
   public float damage = 100f;
   public TowerController tower;
+  public Transform target;
   Vector3 targetOffset = Vector3.up * 0.5f;
   bool hitted = false;
 
   public void SetTower(TowerController tower)
   {
     this.tower = tower;
-  }
-
-  private void FixedUpdate()
-  {
     if (tower.targets.Count == 0)
     {
       StartCoroutine(Explode());
       return;
     }
+    target = tower.targets[0];
+    target.GetComponent<EnemyController>().OnDie.AddListener(UpdateTarget);
+  }
 
+  private void UpdateTarget(Transform oldTarget)
+  {
+    oldTarget.GetComponent<EnemyController>().OnDie.RemoveListener(UpdateTarget);
+
+    if (tower.targets.Count > 0)
+    {
+      target = tower.targets[0];
+      Debug.Break();
+      return;
+    }
+
+    StartCoroutine(Explode());
+  }
+
+  private void FixedUpdate()
+  {
+    if (target == null) return;
     if (DistanceFromTarget() > stopDistance)
     {
       speed += acceleration;
@@ -49,20 +66,17 @@ public class ProjectileController : MonoBehaviour
   private void Hit()
   {
     if (hitted) return;
-    Transform target = tower.targets[0];
     target.GetComponent<EnemyController>().TakeDamage(damage);
     StartCoroutine(Explode());
   }
 
   private Vector3 Direction()
   {
-    Transform target = tower.targets[0];
     return (target.position - transform.position + targetOffset).normalized;
   }
 
   private float DistanceFromTarget()
   {
-    Transform target = tower.targets[0];
     return Vector3.Distance(transform.position, target.position + targetOffset);
   }
 }
